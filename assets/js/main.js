98a7d196dcf9c851bfa480c27b4237f075fc86c1
JarradBaker <jarradbaker@outlook.com>
2023-08-06 18:44:22 +0000

    Add: hamburger menu on click

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const navLinksMobile = document.getElementsByClassName('nav-links-mobile')[0];
+
+/**
+ * Event listener to toggle the hamburger menu and nav links
+ */
+hamburger.addEventListener('click', () => {
+    if(navLinksMobile.getAttribute('class', 'hide')){
+        navLinksMobile.removeAttribute('class', 'hide');
+    } else {
+        navLinksMobile.setAttribute('class', 'hide');
+    } 
+} );
